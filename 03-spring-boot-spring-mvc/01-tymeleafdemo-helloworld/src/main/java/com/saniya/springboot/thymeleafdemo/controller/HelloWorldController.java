ac8404dae381e612fcbd94fbdb987bac33f96bf2
Saniya-Bagwan630 <saniyabagwan630@gmail.com>
2026-06-25 16:03:23 +0530

    use model in controller

--- 03-spring-boot-spring-mvc/01-tymeleafdemo-helloworld/src/main/java/com/saniya/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 03-spring-boot-spring-mvc/01-tymeleafdemo-helloworld/src/main/java/com/saniya/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.saniya.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +19,26 @@ public class HelloWorldController {
     @RequestMapping("/processForm")
     public String processForm(){
         return "helloWorld-process";
+    }
+
+    //controller method to read form data
+    //add data to model
+
+    @RequestMapping("/processFormV2")
+    public String letsShout(HttpServletRequest request, Model themodel){
+
+        //read request parameter from html form
+        String studentName = request.getParameter("studentName");
+
+        //convert name to uppercase
+        studentName = studentName.toUpperCase();
+
+        //create a message
+        String message = "YO !!!!"+studentName;
+
+        //add message to model
+        themodel.addAttribute("message",message);
+
+        return "helloWorld-processV2";
     }
 }

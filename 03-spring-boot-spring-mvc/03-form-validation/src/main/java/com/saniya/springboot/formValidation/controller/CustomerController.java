3fc1e7707afcc0140f8f61cba8cdb819158c0841
Saniya-Bagwan630 <saniyabagwan630@gmail.com>
2026-06-26 22:53:38 +0530

    validation implimentation project

---
+++ 03-spring-boot-spring-mvc/03-form-validation/src/main/java/com/saniya/springboot/formValidation/controller/CustomerController.java
@@ -0,0 +1,48 @@
+package com.saniya.springboot.formValidation.controller;
+
+import com.saniya.springboot.formValidation.model.Customer;
+import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CustomerController {
+
+    //to handle white spaces
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+
+    }
+
+    @GetMapping("/getForm")
+    public String getForm(Model theModel){
+        theModel.addAttribute("customer",new Customer());
+        return "getForm";
+    }
+
+    @PostMapping("/process-form")
+    public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,
+                              BindingResult theBindingResult){
+        System.out.println(theBindingResult.toString());
+
+        //imp for debugging
+        if(theBindingResult.hasErrors()){
+            return "getForm";
+        }else {
+            return "customer-confirmation";
+        }
+    }
+
+
+}
--- 03-spring-boot-spring-mvc/03-form-validation/src/main/java/com/saniya/springboot/formValidation/model/Customer.java
+++ 03-spring-boot-spring-mvc/03-form-validation/src/main/java/com/saniya/springboot/formValidation/model/Customer.java
@@ -1,4 +1,53 @@
 package com.saniya.springboot.formValidation.model;
 
+import jakarta.validation.constraints.*;
+
 public class Customer {
+
+    @NotNull(message = "is required")
+    @Size(min=1, message = "is required")
+    private String fName;
+    private String lName;
+
+    @NotNull(message = "is required")
+    @Min(value = 0 , message="must be greater then or equal to 0")
+    @Max(value = 5, message = "must be less than or equal to 5")
+    private Integer passes;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message="must contain 5 digits/characters")
+    private String postalCode;
+
+    public Customer(){}
+
+    public String getfName() {
+        return fName;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public Integer getPasses() {
+        return passes;
+    }
+
+    public void setPasses(int passes) {
+        this.passes = passes;
+    }
 }
